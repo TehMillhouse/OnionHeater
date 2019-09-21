1bd001263f468e36cd3ec4bb0772740f236075da
Max Wagner <max@trollbu.de>
2019-09-21 13:21:28 +0200

    Implement controller w/ klipper API, factor out model

---
+++ controller.py
@@ -0,0 +1,66 @@
+import model
+from common import *
+
+class HeatController(object):
+    # internally models hotend as made of shells of metal surrounded by air
+    # heater is in innermost shell (0), sensor in outermost metal shell
+    # heat gradients are smoothed, w/ different conductivity b/w air & metal
+
+    def __init__(self, heater, config):
+        self.heater = heater
+        self.heater_max_power = heater.get_max_power()
+        self.printer = config.get_printer()
+        self.config = config
+
+        # heater power in degrees per second
+        self.heater_output = config.getfloat('model_heater_power', 1.68, minval=0)
+        # number of cells in thermal simulation
+        metal_shells = config.getint('model_thermal_mass', 6, minval=2)
+        # minimum number of simulation passes per second
+        passes_per_sec = config.getfloat('model_accuracy', 3)
+        # heat dissipation rate within metal
+        thermal_conductivity = config.getfloat('model_thermal_conductivity', 0.05, minval=0, maxval=1)
+        # heat - air dissipation rate
+        base_cooling = config.getfloat('model_air_cooling', 0.0043445, minval=0, maxval=1)
+        # additional heat dissipation effected by fan at 100%
+        fan_cooling = config.getfloat('model_fan_cooling', base_cooling, minval=0, maxval=(1-base_cooling))
+        initial_temp = 21.5
+
+        self.model = model.Model(self.heater_output, metal_shells, passes_per_sec, initial_temp, thermal_conductivity, base_cooling, fan_cooling)
+        self.current_heater_pwm = 0.0
+        # we keep a tally of how long on avg a control tick lasts
+        self.last_read_times = [-4, -3, -2, -1]
+
+    def stable_state_gradient(self):
+        # TODO: start by initializing the model to 200 degrees, and run it a handfull of seconds
+        # always putting back in what is lost through convection
+        return 0
+
+    def temperature_update(self, read_time, temp, target_temp):
+        self.model.advance_model(read_time, self.current_heater_pwm)
+        self.model.adjust_to_measurement(temp)
+
+        # TODO: Is this really needed?
+        self.last_read_times.append(read_time)
+        self.last_read_times = self.last_read_times[1:]
+        tick_lens = [self.last_read_times[i] - self.last_read_times[i-1] for i in range(1, len(self.last_read_times))]
+        tick_len = sum(tick_lens) / len(tick_lens)
+
+        # now calculate how much heat we still need to dump into the hotend
+        # TODO also add expected thermal egress
+        model_avg_temp = sum(self.model.shells[:-1]) / (len(self.model.shells)-1)
+        degrees_needed = (target_temp - model_avg_temp \
+                # since we're constantly losing heat, there is always an internal gradient in the hotend.
+                # if we don't compensate for this, we'll have a steady state error
+                + self.stable_state_gradient() / 2  \
+                ) * (len(self.model.shells)-1)
+        # if the expected tick length is long, we need less heat
+        self.current_heater_pwm = clamp(degrees_needed / (self.heater_output * tick_len), 0.0, self.heater_max_power)
+        self.heater.set_pwm(read_time, self.current_heater_pwm)
+
+    def clamp(value, lower, upper):
+        return max(lower, min(upper, value))
+
+    def check_busy(self, eventtime, smoothed_temp, target_temp):
+        return abs(smoothed_temp - target_temp) < 15
+
---
+++ model.py
@@ -0,0 +1,64 @@
+
+import math
+from common import *
+
+class Model(object):
+    def __init__(self, heater_power, metal_shells=6, passes_per_sec=3, initial_temp=ENV_TEMP, thermal_conductivity=HEAT_CONDUCT_METAL, base_cooling=HEAT_CONDUCT_AIR, fan_cooling=HEAT_CONDUCT_AIR):
+        """Create thermal model of hotend.
+
+        heater_power: how many degrees per second the heater can output over the hotend
+        metal_shells: how many different locations in the heater block to track. affects
+            thermal mass and dissipation speed in the model
+        passes_per_sec: minimum number of dissipation passes computed per second. Higher is more accurate
+        initial_temp: how hot is the hotend *right now*?
+        """
+        shells = metal_shells + 1  # there's an additional outer shell filled with air
+        self.time = 0
+        self.passes_per_sec = passes_per_sec
+        self.shells = [initial_temp] * shells
+        # Since heat conductivity of metal is pretty high, the heater is effectively outputting
+        # the measured degrees per second over *all* shells
+        self.heater_power = metal_shells * power
+        # heater is at first (innermost) shell, sensor at second-to-last shell, outermost shell is outside
+        self.shells = [initial_temp] * shells
+        self.thermal_conductivity = thermal_conductivity
+        # we have an affine convective cooling model: (base_cooling + fan_power * fan_cooling)
+        # if this turns out to be too simple:
+        # FIXME dynamically adjust the effective wind power based on how much heat we're actually losing
+        self.base_cooling = base_cooling
+        self.fan_cooling = fan_cooling
+
+    def advance_model(self, time, heater_output_until_now, fan_power=0.0):
+        ðt = time - self.time
+        passes = min(1, math.floor(ðt / self.passes_per_sec))
+        for _ in range(passes):
+            self.dissipate_temps(ðt / passes, heater_output_until_now, fan_power)
+        self.time = time
+
+    def adjust_to_measurement(self, sensor_temp):
+        predicted_temp = self.shells[-2]
+        if abs(sensor_temp - predicted_temp) > 3.0:
+            print(f"Model misaligned by {predicted_temp - sensor_temp}")
+        self.shells[-2] = sensor_temp
+
+    def _thermal_conductivity(self, source_idx, target_idx, fan_power):
+        if source_idx == len(self.shells)-1 or target_idx == len(self.shells)-1:
+            return self.base_cooling + fan_power * self.fan_cooling
+        return self.thermal_conductivity
+
+    def dissipate_temps(self, ðt, heater_output, fan_power=1.0):
+        new_shells = list(self.shells)
+        for target in range(len(self.shells)-1):
+            temp_diff = 0
+            for source in [target-1, target+1]:
+                if source < 0 or source >= len(self.shells):
+                    continue
+                dist = abs(source - target)
+                gradient_from_source = self.shells[target] - self.shells[source]
+                thermal_conductivity = self._thermal_conductivity(source, target, fan_power)
+                temp_diff += thermal_conductivity * gradient_from_source / (dist * dist)
+                if target == 0:
+                    # target shell is heater shell
+                    temp_diff += heater_output
+                new_shells[target] = new_shells[target] + ðt * temp_diff
+        self.shells = new_shells
