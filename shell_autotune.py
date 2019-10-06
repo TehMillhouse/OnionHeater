d655ec9f5a124db8dfe4ce82b31bdc87d46c4575
Max Wagner <max@trollbu.de>
2019-10-06 20:13:22 +0200

    WIP on autotune

--- shell_autotune.py
+++ shell_autotune.py
@@ -1,6 +1,6 @@
 # Calibration of model-based controller settings
 
-import math, model
+import math
 
 class Trace:
     def __init__(self):
@@ -88,11 +88,11 @@ class ControlAutoTune:
     env_temp = None
     base_cooling_per_deg = None
     fan_extra_cooling_per_deg = None
-    shells = None
+    shell_conductivity = None
     heater_power = None
 
     # controller phases in order:
-    # heatup, overshoot, cooldown, heatup_fan, overshoot_fan, cooldown_fan
+    phases = ['heatup', 'overshoot', 'cooldown', 'heatup_fan', 'overshoot_fan', 'cooldown_fan']
 
     def __init__(self, heater, target):
         self.heater = heater
@@ -120,9 +120,11 @@ class ControlAutoTune:
         self.temp_samples.append((read_time, temp))
 
         # control code for phases should be listed in reverse order to prevent skipping a phase
+        if self.phase == 'heatup_fan':
+            self.phase = 'done'
         if self.phase == 'cooldown' and temp < target_temp:
             self.heater.alter_target(0)
-            self.phase = 'done'
+            self.phase = 'heatup_fan'
 
         if self.phase == 'overshoot' and temp < last_temp:
             self.phase = 'cooldown'
@@ -140,7 +142,7 @@ class ControlAutoTune:
             self.phase_start[self.phase] = read_time
     def check_busy(self, eventtime, smoothed_temp, target_temp):
         return not self.phase == 'done'
-    # Offline analysis helper
+    # Offline analysis helpers
     def write_file(self, filename):
         pwm = ["pwm: %.3f %.3f" % (time, value)
                for time, value in self.pwm_samples]
@@ -149,6 +151,65 @@ class ControlAutoTune:
         f = open(filename, "wb")
         f.write('\n'.join(pwm + out + phases))
         f.close()
+    def from_file(self, filename='heattest.txt'):
+        self.pwm_samples = []
+        self.temp_samples = []
+        self.phase_start = {}
+        self.phase = 'done'
+        with open(filename, "rb") as f:
+            for line in f.readlines():
+                line = line.decode('utf-8').strip()
+                if line.startswith('pwm: '):
+                    _, time, val = line.split(' ')
+                    self.pwm_samples.append((float(time), float(val)))
+                elif line.startswith('phase '):
+                    _, phase, _, time = line.split(' ')
+                    self.phase_start[phase] = float(time)
+                else:
+                    time, temp = line.split(' ')
+                    self.temp_samples.append((float(time), float(temp)))
+
+    def _lerp(self, a, b, alpha):
+        return a + alpha * (b - a)
+    def calc_params(self):
+        #  These are the variables we need to find
+        self.env_temp = self.temp_samples[0][1]
+        #  base_cooling_per_deg = None
+        #  fan_extra_cooling_per_deg = None
+        #  shell_conductivity = None
+        #  heater_power = None
+
+        #sample_idx = self._middle(*self._get_index_range('cooldown'))
+        start, end = self._get_index_range('cooldown')
+        sample_idx = int(self._lerp(start, end, 0.3))
+
+        return sample_idx
+
+    def _middle(self, idx_1, idx_2):
+        return idx_1 + (idx_2 - idx_1)//2
+
+    def _get_index_range(self, phase):
+        start_time = self.phase_start[phase]
+        end_time = self.phase_start[ self.phases[self.phases.index(phase)+1] ]
+        # O(n) search, could be better
+        start_idx = None
+        end_idx = None
+        for idx, (time, _) in enumerate(self.temp_samples):
+            if start_idx is None and time >= start_time:
+                start_idx = idx
+            if end_idx is None and time >= end_time:
+                end_idx = idx
+                return start_idx, end_idx
+
+
 
 def load_config(config):
     return ShellCalibrate(config)
+
+def get():
+    class FHeater:
+        def get_max_power(self):
+            return 1.0
+    c = ControlAutoTune(FHeater(), 200)
+    c.from_file()
+    return c
